13fa01faca1f03b49768e7deec339ea97f92f745
Chris Mungall <cjm@berkeleybop.org>
2016-08-31 17:41:28 +0100

    adding missing file, should fix travis

---
+++ src/main/java/org/geneontology/obographs/owlapi/SynonymVocabulary.java
@@ -0,0 +1,42 @@
+package org.geneontology.obographs.owlapi;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.geneontology.obographs.model.meta.SynonymPropertyValue.SCOPES;
+
+public class SynonymVocabulary {
+    
+    Map<String,SCOPES> iriToScopeMap = new HashMap<>();
+
+    
+    public SynonymVocabulary() {
+        super();
+        setDefaults();
+    }
+
+    public void setDefaults() {
+        set("http://www.geneontology.org/formats/oboInOwl#hasExactSynonym", SCOPES.EXACT);
+        set("http://www.geneontology.org/formats/oboInOwl#hasRelatedSynonym", SCOPES.RELATED);
+        set("http://www.geneontology.org/formats/oboInOwl#hasNarrowSynonym", SCOPES.NARROW);
+        set("http://www.geneontology.org/formats/oboInOwl#hasBroadSynonym", SCOPES.BROAD);
+    }
+
+    /**
+     * @return the iriToScopeMap
+     */
+    public Map<String, SCOPES> getIriToScopeMap() {
+        return iriToScopeMap;
+    }
+    
+    public void set(String iri, SCOPES scope) {
+        iriToScopeMap.put(iri, scope);
+    }
+    public SCOPES get(String iri) {
+        return iriToScopeMap.get(iri);
+    }
+    public boolean contains(String iri) {
+        return iriToScopeMap.containsKey(iri);
+    }
+
+}
