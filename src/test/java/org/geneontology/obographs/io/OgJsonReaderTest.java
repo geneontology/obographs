6d8cc2f8c522b5571c0b96d9623c04f69fcad424
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2020-02-28 16:34:45 +0000

    Add simple timer to OgJsonReaderTest

--- src/test/java/org/geneontology/obographs/io/OgJsonReaderTest.java
+++ src/test/java/org/geneontology/obographs/io/OgJsonReaderTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.Instant;
 
 /**
  * @author Jules Jacobsen <j.jacobsen@qmul.ac.uk>
@@ -15,8 +17,11 @@ public class OgJsonReaderTest {
 
     @Test
     public void readFile() throws IOException {
+        Instant start = Instant.now();
         Path ontologyPath = Paths.get("src/test/resources/hp.json");
         GraphDocument graphDocument = OgJsonReader.readFile(ontologyPath.toFile());
+        Instant end = Instant.now();
+        System.out.printf("Read %s in %dms%n", ontologyPath, Duration.between(start, end).toMillis());
         Node node = graphDocument.getGraphs().get(0).getNodes().get(0);
         System.out.println(node);
         System.out.println(OgJsonGenerator.render(node));
