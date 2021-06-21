2c0fa0d5e68006d5fc2cebb24b29e3c41cc3f478
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2021-06-21 10:27:50 +0100

    Make FromOwlPerfTest more verbose Add wrapper shell scripts

--- obographs-owlapi/src/test/java/org/geneontology/obographs/owlapi/FromOwlPerfTest.java
+++ obographs-owlapi/src/test/java/org/geneontology/obographs/owlapi/FromOwlPerfTest.java
@@ -15,6 +15,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 public class FromOwlPerfTest {
 
@@ -24,33 +25,37 @@ public class FromOwlPerfTest {
         //String[] exts = {"obo","owl"};
         //File dir = new File("src/test/resources");
         //Collection<File> files = FileUtils.listFiles(dir, exts, true);
-        String[] iris = {
+        List<String> iris = List.of(
                 //"file:///Users/cjm/repos/uberon/uberon.owl",
                 //http://purl.obolibrary.org/obo/uberon.owl"
                 "http://purl.obolibrary.org/obo/ro.owl"
                 //"http://purl.obolibrary.org/obo/mp.owl"      
-        };
+        );
 
         for (String iriString : iris) {
             IRI iri = IRI.create(iriString);
-            System.out.println("Converting: "+iri);
+            System.out.println("Converting: " + iri);
             //System.out.println(file.toPath());
             //System.out.println(file.toString());
 
             OWLOntologyManager m = OWLManager.createOWLOntologyManager();
+            System.out.println("Loading OWL");
             OWLOntology ontology = m.loadOntology(iri);
 
+            String ontologyId = iri.getShortForm().replace(".obo", "").replace(".owl", "");
+            System.out.println("Converting " + ontologyId + " from OWL:");
             FromOwl fromOwl = new FromOwl();
             GraphDocument gd = fromOwl.generateGraphDocument(ontology);
 
-            OgJsonGenerator.write(Files.createTempFile("foo", ".json").toFile(), gd);
-            OgYamlGenerator.write(Files.createTempFile("foo", ".yaml").toFile(), gd);
+            System.out.println("Writing JSON/YAML files...");
+            OgJsonGenerator.write(Files.createTempFile(ontologyId, ".json").toFile(), gd);
+            OgYamlGenerator.write(Files.createTempFile(ontologyId, ".yaml").toFile(), gd);
+            System.out.println("Done");
         }
     }
 
     private void export(String s, Path fn, String suffix) throws IOException {
         String ofn = fn.toString().replace(".obo", suffix).replace(".owl", suffix);
         FileUtils.writeStringToFile(new File("examples/"+ofn), s);
-
     }
 }
